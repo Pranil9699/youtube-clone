fe9f7a95badf17ea8bfe68d41c90f3df7924b7ba
Pranil Takawane <99954777+Pranil9699@users.noreply.github.com>
2024-02-12 17:18:38 +0530

    Add files via upload

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": "841a195ee1msh2eff5c3194ab6f4p1ed384jsn8fae4e79ab09",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options); //Done
+  return data;
+};
